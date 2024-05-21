06766d40657a3c6a970c5112f2623ab1a885b7a2
Jeremy Frankel <jeremyfrankel@ZEPHYRxs-MacBook-Pro.local>
2024-05-21 11:09:37 -0400

    second

---
+++ libs/billing-lib.js
@@ -0,0 +1,12 @@
+export function calculateCost(storage) {
+    //const rate = storage <= 10 ? 4 : storage <= 100 ? 2 : 1;
+    let rate = 0;
+    
+    if(storage <= 10)
+      rate = 4;
+    else if(storage <= 100)
+      rate = 2;
+    else
+      rate = 1;
+    return rate * storage * 100;
+  }
---
+++ list.js
@@ -0,0 +1,22 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: "arn:aws:dynamodb:us-east-2:201933806817:table/notes",
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "userid = :userid",
+    ExpressionAttributeValues: {
+      ":userid": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+  const result = await dynamoDb.query(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
